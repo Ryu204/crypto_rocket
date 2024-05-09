370b6b3e7595acf6b376fa99220e489539cb9088
Ryu204 <nguyenanhbao2356@gmail.com>
2024-05-09 07:36:48 +0700

    make page

--- contract/ignition/modules/CryptoRocket.ts
+++
@@ -1,8 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-const CryptoRocketModule = buildModule("CryptoRocketModule", (m) => {  
-  const lock = m.contract('CryptoRocket');
-  return { lock };
-});
-
-export default CryptoRocketModule;
